12cc4298652c4694786823ad155d9d7814aa9c8a
Nzingani <isabelnzingani@outlook.com>
2018-07-31 08:45:48 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>P5ex5</title>
+  </head>
+  <body>
+    <p>
+  <?php
+   $departement = array ('02' => 'Aisne', '59' => 'Nord', '60' => 'Oise',
+    '62' => 'Pas-de-Calais',' 80' => 'Somme');
+  ?>
+</p>
+</body>
